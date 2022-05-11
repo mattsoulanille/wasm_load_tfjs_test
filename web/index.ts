814a0013b674bab2b513dc07f63194d4bbba22aa
Matthew Soulanille <msoulanille@google.com>
2022-05-11 08:50:03 -0700

    Load file using shared array buffer asynchronously

--- web/index.ts
+++ web/index.ts
@@ -13,13 +13,43 @@ async function main() {
 //  const worker = new Worker('worker_bundle.js');
 //  const MainWasm = Comlink.wrap(worker) as any;
 
-  const sab = new SharedArrayBuffer(1000);
+  // For testing purposes, allocate a large buffer
+  const sab = new SharedArrayBuffer(1e8);
   const worker = new Worker('worker_bundle.js');
   const modelUrl = "https://storage.googleapis.com/tfjs-models/savedmodel/mobilenet_v2_1.0_224/model.json";
   const encoder = new TextEncoder();
-  encoder.encode(modelUrl);
+  const urlEncoded = encoder.encode(modelUrl);
+  const dv = new DataView(sab);
+  const uint8Array = new Uint8Array(sab);
+  uint8Array.set(urlEncoded, 100);
+  dv.setUint32(50, urlEncoded.length);
   worker.postMessage(sab);
 
+  const int32Array = new Int32Array(sab);
+
+  /*
+  while (true) {
+//    if (Atomics.load(int32Array, 0) === 1) {
+    if (int32Array[0] === 1) {
+      break;
+    }
+    //Atomics.wait(int32Array, 0, 1);
+  }
+  */
+  console.log('waiting for response');
+  await (Atomics as any).waitAsync(int32Array, 0, 0).value;
+  console.log('got response');
+  const fileData = uint8Array.slice(2000, int32Array[1] + 2000);
+  const expectedData = await (await fetch(modelUrl)).arrayBuffer();
+  const fileDataUint8 = new Uint8Array(fileData);
+  const expectedDataUint8 = new Uint8Array(expectedData);
+
+  for (let i = 0; i < expectedDataUint8.length; i++) {
+    if (fileDataUint8[i] !== expectedDataUint8[i]) {
+      throw new Error(`${fileDataUint8[i]} != ${expectedDataUint8[i]}`);
+    }
+  }
+  console.log('Loaded the same data');
 
 
   console.log(MainWasm._add(4, 5));
--- web/worker.ts
+++ web/worker.ts
@@ -18,6 +18,22 @@ function getSab(): Promise<SharedArrayBuffer> {
 (async () => {
   const sab = await getSab();
   console.log(sab);
+  const uint8Array = new Uint8Array(sab);
+  const int32Array = new Int32Array(sab);
+  const dv = new DataView(sab);
+  const decoder = new TextDecoder();
+  const url = decoder.decode(uint8Array.slice(100, dv.getUint32(50) + 100));
+  console.log(url);
+
+  const response = await fetch(url);
+  const data = await response.arrayBuffer();
+  const dataUint8 = new Uint8Array(data);
+
+  uint8Array.set(dataUint8, 2000);
+  int32Array[1] = dataUint8.length;
+  Atomics.notify(int32Array, 0);
+//  int32Array[0] = 1;
+
   // const MainWasm = await Init({
   //   locateFile: (f: string) => `cc/main_wasm/${f}`
   // });
